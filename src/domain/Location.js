1789b1ba7bd61b5ae90eb017303dc6de032daf7f
GyeongHo <ludeng0727@gmail.com>
2023-10-05 17:54:12 +0900

    location domain, api 수정

--- src/domain/Location.js
+++ src/domain/Location.js
@@ -1,9 +1,15 @@
 class Location {
     lat;
     lng;
+    address;
+    sido;
+    sigungu;
 
-    constructor(lat,lng) {
+    constructor(lat, lng, address, sido, sigungu) {
         this.lat = lat;
         this.lng = lng;
+        this.address = address;
+        this.sido = sido;
+        this.sigungu = sigungu;
     }
 }
