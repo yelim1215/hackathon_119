9510c727c10bd72c96e57394ea4a7920c6aa8fee
hoon-hoon <kc0393@naver.com>
2023-10-06 00:29:13 +0900

    [MK] api 수정
    입니다

---
+++ src/api_service/apiURL.js
@@ -0,0 +1,7 @@
+export const API_URL = "ErmctInfoInqireService";
+// 병원 응급실 실시간 가용병상정보 조회 오퍼레이션
+export const NO_1 = "/getEmrrmRltmUsefulSckbdInfoInqire";
+// service key
+export const NO_5 = "/getEgytBassInfoInqire";
+export const API_KEY =
+  "7eTdXpvji1tanllfyqPZ%2BMsf%2BM1v1gTbz4N0dsNyFEhV17ahp%2F2n1LYqSMy129Jjc5BTFnNo8vVRYlJOgKzsBw%3D%3D";
