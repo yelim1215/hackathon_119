8f7a65b203c8584c465176eb7fb9915a9ec68646
GyeongHo <ludeng0727@gmail.com>
2023-10-05 23:24:05 +0900

    Location domain, service 수정

--- src/domain/Locution.js
+++ src/domain/Locution.js
@@ -1,11 +1,4 @@
-class Location {
-    lat;
-    lng;
-    address;
-    sido;
-    sigungu;
-
-
+export class Locution {
     constructor(lat, lng, address, sido, sigungu) {
         this.lat = lat;
         this.lng = lng;
