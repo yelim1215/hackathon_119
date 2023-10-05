bfc3d5bbb6ef082abfcd15d25166a9a7320edf15
YuGyeong Park <51533341+sizxero@users.noreply.github.com>
2023-10-06 02:39:03 +0900

    [chore] 누락 코드 취합

--- src/api_service/LocationService.js
+++ src/api_service/LocationService.js
@@ -1,14 +1,36 @@
-class LocationService{
+import { Locution } from "../domain/Locution";
+
+const { kakao } = window
+
+class LocationService {
+
+    geocoder = new kakao.maps.services.Geocoder();
 
     getCurrentLocation = () => {
-        return navigator.geolocation.getCurrentPosition(position => {
-            const lat = position.coords.latitude;
-            const lng = position.coords.longitude;
-            return new Location(lat, lng)
-        })
+        return new Promise((resolve) => {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const lat = position.coords.latitude;
+                const lng = position.coords.longitude;
 
+                this.geocoder.coord2RegionCode(lng, lat, (result, status) => {
+                    if (status === kakao.maps.services.Status.OK) {
+                        resolve(
+                            new Locution(
+                                lat,
+                                lng,
+                                result[0].address_name,
+                                result[0].region_1depth_name,
+                                result[0].region_2depth_name
+                            )
+                        )
+                    }
+                })
+            })
+        })
     }
+
 }
 
 const locationService = new LocationService();
-export default locationService
+
+export default locationService;
